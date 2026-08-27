f0dc2fd5a1e2645662c8223f971ab8b9ef8f4537
Elizabeth Palit <epalit.git@gmail.com>
2026-08-28 00:14:03 +0100

    Lesson 3 - 21: update dealing to calculate hand value

--- lesson_3/twenty_one/twenty_one.py
+++ lesson_3/twenty_one/twenty_one.py
@@ -40,10 +40,57 @@ def get_card(deck):
     return deck.pop()
 
 def deal(hand, deck):
-    hand.append(get_card(deck))
+    hand['cards'].append(get_card(deck))
+    update_hand(hand)
+
+def update_hand(hand):
+    if has_ace(hand):
+        set_ace_values(hand)
+    else:
+        recalculate_hand_total(hand)
+
+def has_ace(hand):
+    for card in hand['cards']:
+        if card['name'] == 'Ace':
+            return True
+    return False
+
+def recalculate_hand_total(hand):
+    total = calculate_hand_total(hand)
+    hand['total'] = total
+
+def set_ace_values(hand):
+    aces = []
+
+    # set all aces to 1
+    for card in hand['cards']:
+        if card['name'] == 'Ace':
+            card['value'] = 1
+            aces.append(card)
+    recalculate_hand_total(hand)
+
+    # if hand is bust with all aces set to 1, leave it as is
+    if is_bust(hand):
+        return
+
+    # can only have 1 ace with value 11 per hand (11 + 11 = 22 which is bust)
+    if aces:
+        aces[0]['value'] = 11 # arbitrarily pick first ace to set to 11
+        recalculate_hand_total(hand)
+        if is_bust(hand):
+            aces[0]['value'] = 1 # if setting to 11 busts the hand keep it as 1
+            recalculate_hand_total(hand)
+
+def calculate_hand_total(hand):
+    return sum([card['value'] for card in hand['cards']])
+
+def is_bust(hand):
+    return hand['total'] > 21
 
 def get_initial_hands():
-    return [], []
+    player_hand = {'cards': [], 'value': 0}
+    dealer_hand = {'cards': [], 'value': 0}
+    return player_hand, dealer_hand
 
 def do_first_deal(player_hand, dealer_hand, deck):
     for _ in range(INITIAL_HAND_SIZE):
