b7e6d4c2e0c770edb8d5683b338f5f6f13fba32c
Elizabeth Palit <epalit.git@gmail.com>
2026-09-11 14:25:57 +0100

    Small Problems - Easy 2: 6. Multiplicative Average

---
+++ exercises/easy_2/exercise_6/solution.py
@@ -0,0 +1,15 @@
+def multiplicative_average(nums):
+    total = 1
+    for num in nums:
+        total *= num
+    
+    result = total / len(nums)
+    
+    return f"{result:.3f}"
+
+# All of these examples should print True
+print(multiplicative_average([3, 5]) == "7.500")
+print(multiplicative_average([2, 5, 8]) == "26.667")
+print(multiplicative_average([2, 5]) == "5.000")
+print(multiplicative_average([1, 1, 1, 1]) == "0.250")
+print(multiplicative_average([2, 5, 7, 11, 13, 17]) == "28361.667")
