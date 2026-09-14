0219c0dc9797ee4f49f089b7e3828d4d0a4cc28e
Elizabeth Palit <epalit.git@gmail.com>
2026-09-14 11:23:45 +0100

    Small Problems - Easy 2: 8. List of Digits

---
+++ exercises/easy_2/exercise_8/solution.py
@@ -0,0 +1,22 @@
+"""
+# P
+input: integer
+output: list of digits in the number
+
+requirements:
+- output list contains ints
+
+# E
+print(digit_list(12345) == [1, 2, 3, 4, 5])       # True
+print(digit_list(7) == [7])                       # True
+print(digit_list(375290) == [3, 7, 5, 2, 9, 0])   # True
+print(digit_list(444) == [4, 4, 4])               # True
+"""
+
+def digit_list(num):
+    return [int(digit) for digit in str(num)]
+
+print(digit_list(12345) == [1, 2, 3, 4, 5])       # True
+print(digit_list(7) == [7])                       # True
+print(digit_list(375290) == [3, 7, 5, 2, 9, 0])   # True
+print(digit_list(444) == [4, 4, 4])               # True
