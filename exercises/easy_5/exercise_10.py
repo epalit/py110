7eb811339b753c6bc12d78cc1eee04060ddb9815
Elizabeth Palit <epalit.git@gmail.com>
2026-09-16 21:57:48 +0100

    Small Problems - Easy 5: 10. Remove Consecutive Duplicates

---
+++ exercises/easy_5/exercise_10.py
@@ -0,0 +1,17 @@
+def unique_sequence(sequence):
+    result = []
+    previous_value = None
+    for num in sequence:
+        if num != previous_value:
+            result.append(num)
+        previous_value = num
+    return result
+
+original = [1, 1, 2, 6, 6, 6, 5, 5, 3, 3, 3, 4]
+expected = [1, 2, 6, 5, 3, 4]
+print(unique_sequence(original) == expected)      # True
+
+# Non-consecutive duplicates are kept
+original = [1, 2, 1, 3]
+expected = [1, 2, 1, 3]
+print(unique_sequence(original) == expected)      # True
