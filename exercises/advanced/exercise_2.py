d2ff0c7f2e8a62f3fa01d5edb134307e3e712e20
Elizabeth Palit <epalit.git@gmail.com>
2026-09-23 14:04:48 +0100

    Small Problems - Advanced: 2. Transpose MxN Matrix

---
+++ exercises/advanced/exercise_2.py
@@ -0,0 +1,28 @@
+def transpose(matrix):
+    output = [[] for _ in range(len(matrix[0]))]
+
+    for row in matrix:
+        for idx, element in enumerate(row):
+            output[idx].append(element)
+
+    return output
+
+# All of these examples should print True
+print(transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]])
+print(transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]])
+print(transpose([[1]]) == [[1]])
+
+matrix_3_by_5 = [
+    [1, 2, 3, 4, 5],
+    [4, 3, 2, 1, 0],
+    [3, 7, 8, 6, 2],
+]
+expected_result = [
+    [1, 4, 3],
+    [2, 3, 7],
+    [3, 2, 8],
+    [4, 1, 6],
+    [5, 0, 2],
+]
+
+print(transpose(matrix_3_by_5) == expected_result)
